f70e9aabb6f59edf821dc5d50fa125fa98cf2660
Taro Fukunaga <tarof429@gmail.com>
2024-10-30 17:41:06 +0000

    Update Rocky 9 manual install

--- kvm_virt_install_manual_rocky9/install.sh
+++ kvm_virt_install_manual_rocky9/install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
  sudo virt-install \
-    --virt-type=kvm \
-    --name rocky9-test \
-    --ram  4096 \
-    --vcpus=4 \
-    --os-variant=rocky9 \
-    --cdrom=/data/libvirt/default/boot/Rocky-9.4-x86_64-minimal.iso \
-    --network=bridge=br0,model=virtio \
-    --graphics vnc \
-    --disk path=/data/libvirt/default/images/rocky9-test.qcow2,size=40,bus=virtio,format=qcow2
+   --virt-type=kvm \
+   --name rocky9-test \
+   --ram  4096 \
+   --vcpus=4 \
+   --os-variant=rocky9 \
+   --cdrom=/data/libvirt/default/boot/Rocky-9.4-x86_64-minimal.iso \
+   --network=bridge=br0,model=virtio \
+   --graphics vnc \
+   --disk path=/data/libvirt/default/images/rocky9-test.qcow2,size=40,bus=virtio,format=qcow2
